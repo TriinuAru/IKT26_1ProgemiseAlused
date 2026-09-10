1a6e91c0751e97f8cd0687d48d7066cdf3778a5c
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 10:51:59 +0300

    If and Else (Numbers)

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine("Sisestasid numbri:");
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("ERROR.Numbrit ei sisestatud");
+            }
+        }
+    }
+}
