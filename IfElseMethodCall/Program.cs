0080d544885c5d8443525e4433eab6c3663e8dcb
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 10:53:25 +0300

    24.09.26

---
+++ IfElseMethodCall/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfElseMethodCall
+{
+    internal class Program
+    {
+        //main on meetod, mis läheb alati esimesena tööle
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("Hello, World!");
+            //kasutada if ja else
+            //kui kasutaja soovib, siis saab meetodid ta välja kutsuda
+            Console.WriteLine("Ütle meow.");
+            string Input = Console.ReadLine();
+
+            if (Input == "meow")
+            {
+                HelloMethod();
+            }
+            else
+            {
+                Console.WriteLine(">:(");
+            }
+        }
+
+        //teha teine meetod siia ja nimeks on sellel HelloMethod
+        static void HelloMethod()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Hello Kitty! :3");
+        }
+    }
+}
